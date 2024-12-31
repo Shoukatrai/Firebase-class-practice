9cf1da0f37882e3f946417fd76699a5dada664f8
Shoukatrai <raishoukat033@gmail.com>
2024-12-31 14:33:36 +0500

    connecting firebase

---
+++ class 02/firebase.js
@@ -0,0 +1,22 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyBS0S8MBgCrfInHZZF8R8oqYH3x699GGzQ",
+   authDomain: "test-project-c397d.firebaseapp.com",
+   projectId: "test-project-c397d",
+   storageBucket: "test-project-c397d.firebasestorage.app",
+   messagingSenderId: "390964343700",
+   appId: "1:390964343700:web:57358e1661ddeecc131581"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+
+export {
+    app
+}
