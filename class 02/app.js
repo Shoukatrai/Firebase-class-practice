38531fe2809b6424841ba9c4927a609d601869ee
Shoukatrai <raishoukat033@gmail.com>
2024-12-31 15:26:11 +0500

    adding and reading data successful

--- class 02/app.js
+++ class 02/app.js
@@ -1,2 +1,53 @@
-import { app } from "./firebase.js";
-console.log(app)
+import { addDoc, app, collection, db, getDocs } from "./firebase.js";
+
+//getting elements from html
+const inputNote = document.querySelector("#inputNote");
+const cardParent = document.querySelector("#cardParent")
+//adding data
+const addNote = async ()=>{
+    try {
+     if(!inputNote.value){
+        alert("Enter any task..")
+        return
+     }
+    const noteObj = {
+        todo:inputNote.value,
+    }
+    
+    // const addingData = await addDoc(newCollection , {
+    //     newNote: inputNote.value
+    // })
+    const addingData = await addDoc(collection(db, "newNote"), noteObj)
+    inputNote.value = ""
+    console.log(addingData)  
+    fetchingData() 
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+
+const fetchingData = async ()=>{
+    try {
+        const querySnapshot = await getDocs(collection(db, "newNote"));
+        cardParent.innerHTML = ""
+        querySnapshot.forEach((docs)=>{
+            console.log(docs.id)
+            console.log(docs.data())
+            //render UI
+            cardParent.innerHTML += `<div class="card p-3 mt-3 d-flex justify-content-between">
+            ${docs.data().todo}
+          <div class="btns">
+              <button class="me-3 btn btn-info">EDIT</button>
+              <button class="me-3 btn btn-danger">DELETE</button>
+          </div>
+        </div>` 
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+window.addNote = addNote
+window.fetchingData = fetchingData 
--- class 02/firebase.js
+++ class 02/firebase.js
@@ -1,5 +1,6 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+ import { getFirestore , collection, addDoc , getDocs} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,8 +16,12 @@
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
-
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
 export {
-    app
+    app,
+    db,
+    collection, addDoc,
+    getDocs
 }
